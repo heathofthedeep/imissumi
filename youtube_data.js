271fc80b1928e82d571e1d2e979cfb61180fd992
Heath Anderson <heath@heathanderson.net>
2025-02-10 20:14:26 -0600

    initial commit

---
+++ youtube_data.js
@@ -0,0 +1,8 @@
+export const online = false;
+
+export const videoDate = new Date("2025-02-10T02:25:37Z");
+export const videoHref = "https://www.youtube.com/watch?v=cc_AUG01YCA";
+export const videoTitle = "sam &amp; max hit the road";
+
+export const upcomingHref = "https://www.youtube.com/watch?v=JlZpS3jsQh0";
+export const upcomingTitle = "NEW OUTFIT DEBUT";
