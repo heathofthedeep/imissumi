39df84a10213913e48bfaa8eafc8b4be90240d74
heathofthedeep <neap-noisome-2b@icloud.com>
2025-02-13 22:46:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,6 +4,6 @@ export const online = false;
     export const videoHref = "https://www.youtube.com/watch?v=xjkGo-6fgLs";
     export const videoTitle = "【LUCID KARAOKE RELAY】like you have a choice to love me or not";
     
-    export const upcomingHref = "https://www.youtube.com/watch?v=gMSd6jnBIlQ";
-    export const upcomingTitle = "i don&#39;t like valentine&#39;s but i&#39;d let you...";
+    export const upcomingHref = "https://www.youtube.com/watch?v=no2yvmGhBOw";
+    export const upcomingTitle = "TUNNEL OF LOVE【VIRTUAL HEARTS VISUAL NOVEL RELAY】";
     
