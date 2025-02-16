17acd184dbc8ebf4c6e1e10a065491e21a1f12aa
heathofthedeep <neap-noisome-2b@icloud.com>
2025-02-15 19:16:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,9 +1,9 @@
-export const online = true;
+export const online = false;
     
-    export const videoDate = new Date("2025-02-13T01:40:14Z");
-    export const videoHref = "https://www.youtube.com/watch?v=xjkGo-6fgLs";
-    export const videoTitle = "【LUCID KARAOKE RELAY】like you have a choice to love me or not";
+    export const videoDate = new Date("2025-02-16T01:01:41Z");
+    export const videoHref = "https://www.youtube.com/watch?v=gMSd6jnBIlQ";
+    export const videoTitle = "i don&#39;t like valentine&#39;s...";
     
-    export const upcomingHref = "https://www.youtube.com/watch?v=gMSd6jnBIlQ";
-    export const upcomingTitle = "i don&#39;t like valentine&#39;s...";
+    export const upcomingHref = "https://www.youtube.com/watch?v=-y6RoBlm6Gs";
+    export const upcomingTitle = "i&#39;m your yandere internet girlfriend【MILLENIUM WHISPER】";
     
