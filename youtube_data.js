9e2f1ce5728f8cabb7c70fa173f9c6506f2ea478
heathofthedeep <neap-noisome-2b@icloud.com>
2025-02-16 17:16:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,9 +1,9 @@
-export const online = false;
+export const online = true;
     
     export const videoDate = new Date("2025-02-16T01:01:41Z");
     export const videoHref = "https://www.youtube.com/watch?v=gMSd6jnBIlQ";
     export const videoTitle = "i don&#39;t like valentine&#39;s...";
     
-    export const upcomingHref = "https://www.youtube.com/watch?v=WCUuy2YrP2M";
-    export const upcomingTitle = "wet wednesday";
+    export const upcomingHref = "https://www.youtube.com/watch?v=no2yvmGhBOw";
+    export const upcomingTitle = "TUNNEL OF LOVE【VIRTUAL HEARTS VISUAL NOVEL RELAY】";
     
