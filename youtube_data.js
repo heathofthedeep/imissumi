8b40879df8b81c6351187f537581d65fe5f485f7
heathofthedeep <neap-noisome-2b@icloud.com>
2025-02-18 21:16:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,8 +1,8 @@
 export const online = false;
     
-export const videoDate = new Date("2025-02-16T23:28:15Z");
-export const videoHref = "https://www.youtube.com/watch?v=no2yvmGhBOw";
-export const videoTitle = "TUNNEL OF LOVE【VIRTUAL HEARTS VISUAL NOVEL RELAY】";
+export const videoDate = new Date("2025-02-19T02:47:24Z");
+export const videoHref = "https://www.youtube.com/watch?v=j49U8Ea167c";
+export const videoTitle = "wanna rev up your umi? vroom vroom";
 
 export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=WCUuy2YrP2M","title":"wet wednesday","thumbnail":"https://i.ytimg.com/vi/WCUuy2YrP2M/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=-y6RoBlm6Gs","title":"i&#39;m your yandere internet girlfriend【MILLENIUM WHISPER】","thumbnail":"https://i.ytimg.com/vi/-y6RoBlm6Gs/mqdefault_live.jpg"}];
 
