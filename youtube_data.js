7bda32f65d6b237a3a975065d7e1c16f6f9bb86b
heathofthedeep <neap-noisome-2b@icloud.com>
2025-02-19 20:46:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,8 +1,8 @@
 export const online = false;
     
-export const videoDate = new Date("2025-02-19T02:47:24Z");
-export const videoHref = "https://www.youtube.com/watch?v=j49U8Ea167c";
-export const videoTitle = "wanna rev up your umi? vroom vroom";
+export const videoDate = new Date("2025-02-20T02:22:28Z");
+export const videoHref = "https://www.youtube.com/watch?v=WCUuy2YrP2M";
+export const videoTitle = "wet wednesday";
 
 export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=-y6RoBlm6Gs","title":"i&#39;m your yandere internet girlfriend【MILLENIUM WHISPER】","thumbnail":"https://i.ytimg.com/vi/-y6RoBlm6Gs/mqdefault_live.jpg"}];
 
