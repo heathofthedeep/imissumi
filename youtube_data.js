abddab1dd889ca5968d47e704c2dfe4c1a1da88a
heathofthedeep <neap-noisome-2b@icloud.com>
2025-02-23 14:16:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = true;
+export const online = false;
     
-export const videoDate = new Date("2025-02-20T02:22:28Z");
-export const videoHref = "https://www.youtube.com/watch?v=WCUuy2YrP2M";
-export const videoTitle = "wet wednesday";
+export const videoDate = new Date("2025-02-23T20:06:07Z");
+export const videoHref = "https://www.youtube.com/watch?v=-y6RoBlm6Gs";
+export const videoTitle = "yandere internet girlfriend";
 
 export const upcomingVideos = [];
 
-export const liveHref = "https://www.youtube.com/watch?v=-y6RoBlm6Gs";
-export const liveTitle = "yandere internet girlfriend";
+export const liveHref = "";
+export const liveTitle = "";
