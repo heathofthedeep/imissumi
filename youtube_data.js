b079f7b199764db3bebf42392d64de88992f4567
heathofthedeep <neap-noisome-2b@icloud.com>
2025-02-27 22:16:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-02-25T21:00:15Z");
 export const videoHref = "https://www.youtube.com/watch?v=DtWimStDP-U";
 export const videoTitle = "Endless Blue - Umi Kyoku (8Bit Cover)";
 
-export const upcomingVideos = [];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=gh0TDkriOg8","title":"i’m still on vacation with @BriAtCookiebox","thumbnail":"https://i.ytimg.com/vi/gh0TDkriOg8/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
