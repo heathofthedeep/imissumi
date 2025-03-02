5a07c213c622085c994e4c8b236e1c3747de52bd
heathofthedeep <neap-noisome-2b@icloud.com>
2025-03-02 14:16:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,8 +1,8 @@
 export const online = false;
     
-export const videoDate = new Date("2025-02-28T20:00:25Z");
-export const videoHref = "https://www.youtube.com/watch?v=Q6fkzecTNE0";
-export const videoTitle = "Taste me, too #shorts";
+export const videoDate = new Date("2025-03-02T19:47:00Z");
+export const videoHref = "https://www.youtube.com/watch?v=gh0TDkriOg8";
+export const videoTitle = "i’m still on vacation with @BriAtCookiebox and @izumiteeny";
 
 export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=-WTpAJuXYfc","title":"will u touch the booty","thumbnail":"https://i.ytimg.com/vi/-WTpAJuXYfc/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=zyru4ISmE-8","title":"hole","thumbnail":"https://i.ytimg.com/vi/zyru4ISmE-8/mqdefault_live.jpg"}];
 
