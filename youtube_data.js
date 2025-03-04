97472864db4ca1cafc4fbc2e21e0035cf3b5c242
heathofthedeep <neap-noisome-2b@icloud.com>
2025-03-04 15:46:07 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-03-02T19:47:00Z");
 export const videoHref = "https://www.youtube.com/watch?v=gh0TDkriOg8";
 export const videoTitle = "i’m still on vacation with @BriAtCookiebox and @izumiteeny";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=-WTpAJuXYfc","title":"will u touch the booty","thumbnail":"https://i.ytimg.com/vi/-WTpAJuXYfc/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=zyru4ISmE-8","title":"hole","thumbnail":"https://i.ytimg.com/vi/zyru4ISmE-8/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=zyru4ISmE-8","title":"hole","thumbnail":"https://i.ytimg.com/vi/zyru4ISmE-8/mqdefault_live.jpg"}];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=-WTpAJuXYfc";
+export const liveTitle = "will u touch the booty";
