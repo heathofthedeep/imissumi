5ca1120ff732d85c4416c28564c7c56b071747cb
heathofthedeep <neap-noisome-2b@icloud.com>
2025-03-08 23:16:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-03-09T00:58:52Z");
 export const videoHref = "https://www.youtube.com/watch?v=zyru4ISmE-8";
 export const videoTitle = "naming 100 women challenge";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=K-EGCnhsb_g","title":"how appropriate you fight like a cow (monkey island)","thumbnail":"https://i.ytimg.com/vi/K-EGCnhsb_g/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=ZSo1_w49-TQ","title":"tummy wars","thumbnail":"https://i.ytimg.com/vi/ZSo1_w49-TQ/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=_Cnd0zkuiVM","title":"more fast food simulator","thumbnail":"https://i.ytimg.com/vi/_Cnd0zkuiVM/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=K-EGCnhsb_g","title":"how appropriate you fight like a cow (monkey island)","thumbnail":"https://i.ytimg.com/vi/K-EGCnhsb_g/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=ZSo1_w49-TQ","title":"tummy wars","thumbnail":"https://i.ytimg.com/vi/ZSo1_w49-TQ/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
