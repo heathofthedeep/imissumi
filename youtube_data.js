5c531a2711615ed12c7821a3739020416f842fba
heathofthedeep <neap-noisome-2b@icloud.com>
2025-03-08 19:16:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = true;
+export const online = false;
     
-export const videoDate = new Date("2025-03-05T02:02:15Z");
-export const videoHref = "https://www.youtube.com/watch?v=-WTpAJuXYfc";
-export const videoTitle = "will u touch the booty (monkey island)";
+export const videoDate = new Date("2025-03-09T00:58:52Z");
+export const videoHref = "https://www.youtube.com/watch?v=zyru4ISmE-8";
+export const videoTitle = "naming 100 women challenge";
 
 export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=K-EGCnhsb_g","title":"how appropriate you fight like a cow (monkey island)","thumbnail":"https://i.ytimg.com/vi/K-EGCnhsb_g/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=ZSo1_w49-TQ","title":"tummy wars","thumbnail":"https://i.ytimg.com/vi/ZSo1_w49-TQ/mqdefault_live.jpg"}];
 
-export const liveHref = "https://www.youtube.com/watch?v=zyru4ISmE-8";
-export const liveTitle = "naming 100 women challenge";
+export const liveHref = "";
+export const liveTitle = "";
