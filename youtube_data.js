ab01eac0c5855185992050375800762d8e5ca6bc
heathofthedeep <neap-noisome-2b@icloud.com>
2025-03-10 14:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,8 +1,8 @@
 export const online = false;
     
-export const videoDate = new Date("2025-03-09T19:13:02Z");
-export const videoHref = "https://www.youtube.com/watch?v=_Cnd0zkuiVM";
-export const videoTitle = "more fast food simulator";
+export const videoDate = new Date("2025-03-10T19:00:30Z");
+export const videoHref = "https://www.youtube.com/watch?v=TludpZIHUxg";
+export const videoTitle = "tummy";
 
 export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=K-EGCnhsb_g","title":"how appropriate you fight like a cow (monkey island)","thumbnail":"https://i.ytimg.com/vi/K-EGCnhsb_g/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=ZSo1_w49-TQ","title":"tummy wars","thumbnail":"https://i.ytimg.com/vi/ZSo1_w49-TQ/mqdefault_live.jpg"}];
 
