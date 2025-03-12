dd3edfc4b0a54fb52210fa0606df38a8ef5bb9bd
heathofthedeep <neap-noisome-2b@icloud.com>
2025-03-12 16:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-03-10T19:00:30Z");
 export const videoHref = "https://www.youtube.com/watch?v=TludpZIHUxg";
 export const videoTitle = "tummy";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=K-EGCnhsb_g","title":"how appropriate you fight like a cow (monkey island)","thumbnail":"https://i.ytimg.com/vi/K-EGCnhsb_g/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=ZSo1_w49-TQ","title":"tummy wars","thumbnail":"https://i.ytimg.com/vi/ZSo1_w49-TQ/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=ZSo1_w49-TQ","title":"tummy wars","thumbnail":"https://i.ytimg.com/vi/ZSo1_w49-TQ/mqdefault_live.jpg"}];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=K-EGCnhsb_g";
+export const liveTitle = "how appropriate you fight like a cow (monkey island)";
