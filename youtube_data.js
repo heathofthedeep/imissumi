0149c114648d68f07c1b2c6c55199ee77b4e63d4
heathofthedeep <neap-noisome-2b@icloud.com>
2025-03-20 13:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-03-18T23:49:39Z");
 export const videoHref = "https://www.youtube.com/watch?v=ZSo1_w49-TQ";
 export const videoTitle = "tummy wars";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=x4i50XvL1Gw","title":"cuddle with me?","thumbnail":"https://i.ytimg.com/vi/x4i50XvL1Gw/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=w0oy_fsQNWE","title":"put sunblock on me, we&#39;re going sailing (monkey island)","thumbnail":"https://i.ytimg.com/vi/w0oy_fsQNWE/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=6hi3S4ZoWBQ","title":"i sing u listen (unarchived karaoke)","thumbnail":"https://i.ytimg.com/vi/6hi3S4ZoWBQ/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=x4i50XvL1Gw","title":"cuddle with me?","thumbnail":"https://i.ytimg.com/vi/x4i50XvL1Gw/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=w0oy_fsQNWE","title":"put sunblock on me, we&#39;re going sailing (monkey island)","thumbnail":"https://i.ytimg.com/vi/w0oy_fsQNWE/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
