248bbc6f12660e969850b9d0f03c24325b8c9b64
heathofthedeep <neap-noisome-2b@icloud.com>
2025-03-22 15:46:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-03-22T00:37:44Z");
 export const videoHref = "https://www.youtube.com/watch?v=w0oy_fsQNWE";
 export const videoTitle = "put sunblock on me, we&#39;re going sailing (monkey island)";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=6hi3S4ZoWBQ","title":"i sing u listen (unarchived karaoke)","thumbnail":"https://i.ytimg.com/vi/6hi3S4ZoWBQ/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=x4i50XvL1Gw","title":"cuddle with me?","thumbnail":"https://i.ytimg.com/vi/x4i50XvL1Gw/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=x4i50XvL1Gw","title":"cuddle with me?","thumbnail":"https://i.ytimg.com/vi/x4i50XvL1Gw/mqdefault_live.jpg"}];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=6hi3S4ZoWBQ";
+export const liveTitle = "i sing u listen (unarchived karaoke)";
