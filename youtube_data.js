e25ed112a68e09a70c35ffbbae0841c1f28ab079
heathofthedeep <neap-noisome-2b@icloud.com>
2025-03-23 15:46:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-03-22T00:37:44Z");
 export const videoHref = "https://www.youtube.com/watch?v=w0oy_fsQNWE";
 export const videoTitle = "put sunblock on me, we&#39;re going sailing (monkey island)";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=PUar380YpkQ","title":"would you like breast milk in your latte?","thumbnail":"https://i.ytimg.com/vi/PUar380YpkQ/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=x4i50XvL1Gw","title":"cuddle with me?","thumbnail":"https://i.ytimg.com/vi/x4i50XvL1Gw/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=urke5xzjvmI","title":"meat beating: no more horni","thumbnail":"https://i.ytimg.com/vi/urke5xzjvmI/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=PUar380YpkQ","title":"would you like breast milk in your latte?","thumbnail":"https://i.ytimg.com/vi/PUar380YpkQ/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=x4i50XvL1Gw","title":"cuddle with me?","thumbnail":"https://i.ytimg.com/vi/x4i50XvL1Gw/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
