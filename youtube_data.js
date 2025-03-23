6fab37fbbecbb63d22005440301779afeb3d02c1
heathofthedeep <neap-noisome-2b@icloud.com>
2025-03-22 19:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,4 +1,4 @@
-export const online = true;
+export const online = false;
     
 export const videoDate = new Date("2025-03-22T00:37:44Z");
 export const videoHref = "https://www.youtube.com/watch?v=w0oy_fsQNWE";
@@ -6,5 +6,5 @@ export const videoTitle = "put sunblock on me, we&#39;re going sailing (monkey i
 
 export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=x4i50XvL1Gw","title":"cuddle with me?","thumbnail":"https://i.ytimg.com/vi/x4i50XvL1Gw/mqdefault_live.jpg"}];
 
-export const liveHref = "https://www.youtube.com/watch?v=6hi3S4ZoWBQ";
-export const liveTitle = "i sing u listen (unarchived karaoke)";
+export const liveHref = "";
+export const liveTitle = "";
