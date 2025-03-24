a17ccf0281d39a3e3d69445ea09d1306c203d620
heathofthedeep <neap-noisome-2b@icloud.com>
2025-03-23 20:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,8 +1,8 @@
 export const online = false;
     
-export const videoDate = new Date("2025-03-22T00:37:44Z");
-export const videoHref = "https://www.youtube.com/watch?v=w0oy_fsQNWE";
-export const videoTitle = "put sunblock on me, we&#39;re going sailing (monkey island)";
+export const videoDate = new Date("2025-03-24T00:47:12Z");
+export const videoHref = "https://www.youtube.com/watch?v=x4i50XvL1Gw";
+export const videoTitle = "cuddle with me?";
 
 export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=urke5xzjvmI","title":"meat beating: no more horni","thumbnail":"https://i.ytimg.com/vi/urke5xzjvmI/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=PUar380YpkQ","title":"would you like breast milk in your latte?","thumbnail":"https://i.ytimg.com/vi/PUar380YpkQ/mqdefault_live.jpg"}];
 
