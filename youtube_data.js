5560c42cfabb089e4d8434685d01107d9512f752
heathofthedeep <neap-noisome-2b@icloud.com>
2025-03-25 21:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-03-24T00:47:12Z");
 export const videoHref = "https://www.youtube.com/watch?v=x4i50XvL1Gw";
 export const videoTitle = "cuddle with me?";
 
-export const upcomingVideos = [];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=Fe3bYoBqSAg","title":"i dare u to touch the butt","thumbnail":"https://i.ytimg.com/vi/Fe3bYoBqSAg/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=urke5xzjvmI","title":"meat beating: no more horni","thumbnail":"https://i.ytimg.com/vi/urke5xzjvmI/mqdefault_live.jpg"}];
 
 export const liveHref = "https://www.youtube.com/watch?v=PUar380YpkQ";
 export const liveTitle = "would you like breast milk in your latte?";
