99628695e2d2dd91ed05f37669fcaa37b0651a04
heathofthedeep <neap-noisome-2b@icloud.com>
2025-03-25 20:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-03-24T00:47:12Z");
 export const videoHref = "https://www.youtube.com/watch?v=x4i50XvL1Gw";
 export const videoTitle = "cuddle with me?";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=PUar380YpkQ","title":"would you like breast milk in your latte?","thumbnail":"https://i.ytimg.com/vi/PUar380YpkQ/mqdefault_live.jpg"}];
+export const upcomingVideos = [];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=PUar380YpkQ";
+export const liveTitle = "would you like breast milk in your latte?";
