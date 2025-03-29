1a6bcb6c4a4554cb6a248489b68f5fb4523fce7c
heathofthedeep <neap-noisome-2b@icloud.com>
2025-03-29 16:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-03-28T21:55:42Z");
 export const videoHref = "https://www.youtube.com/watch?v=39uoP9sRM4I";
 export const videoTitle = "this guy drowned while i sang";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=flW-Q_rAGxo","title":"one iced latte w ur milk pls","thumbnail":"https://i.ytimg.com/vi/flW-Q_rAGxo/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=Fe3bYoBqSAg","title":"i dare u to touch the butt","thumbnail":"https://i.ytimg.com/vi/Fe3bYoBqSAg/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=urke5xzjvmI","title":"meat beating: no more horni","thumbnail":"https://i.ytimg.com/vi/urke5xzjvmI/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=flW-Q_rAGxo","title":"one iced latte w ur milk pls","thumbnail":"https://i.ytimg.com/vi/flW-Q_rAGxo/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=urke5xzjvmI","title":"meat beating: no more horni","thumbnail":"https://i.ytimg.com/vi/urke5xzjvmI/mqdefault_live.jpg"}];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=Fe3bYoBqSAg";
+export const liveTitle = "your GF ignores you to play video games";
