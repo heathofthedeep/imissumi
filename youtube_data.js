ffd8eef85b6280efebd9ac2e07c1d0b53014af55
heathofthedeep <neap-noisome-2b@icloud.com>
2025-04-05 13:46:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-03-30T19:12:10Z");
 export const videoHref = "https://www.youtube.com/watch?v=urke5xzjvmI";
 export const videoTitle = "meat beating: no more horni";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=flW-Q_rAGxo","title":"one iced latte w ur milk pls","thumbnail":"https://i.ytimg.com/vi/flW-Q_rAGxo/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=PrENYq4nPQE","title":"lonely","thumbnail":"https://i.ytimg.com/vi/PrENYq4nPQE/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=1oq1P69CvKo","title":"dragons fall in love too (minecraft collab)","thumbnail":"https://i.ytimg.com/vi/1oq1P69CvKo/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=flW-Q_rAGxo","title":"one iced latte w ur milk pls","thumbnail":"https://i.ytimg.com/vi/flW-Q_rAGxo/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
