080f9c71390b5108e8c5245c2d91b7fae4577b06
heathofthedeep <neap-noisome-2b@icloud.com>
2025-04-06 17:16:07 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-04-06T01:06:23Z");
 export const videoHref = "https://www.youtube.com/watch?v=flW-Q_rAGxo";
 export const videoTitle = "one iced latte w ur milk pls";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=PrENYq4nPQE","title":"lonely","thumbnail":"https://i.ytimg.com/vi/PrENYq4nPQE/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=1oq1P69CvKo","title":"dragons fall in love too (minecraft collab)","thumbnail":"https://i.ytimg.com/vi/1oq1P69CvKo/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=PrENYq4nPQE","title":"lonely","thumbnail":"https://i.ytimg.com/vi/PrENYq4nPQE/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
