ca1c16d358e541cc64e4f6d649419ad807bcf186
heathofthedeep <neap-noisome-2b@icloud.com>
2025-04-08 19:16:07 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,8 +1,8 @@
 export const online = false;
     
-export const videoDate = new Date("2025-04-06T01:06:23Z");
-export const videoHref = "https://www.youtube.com/watch?v=flW-Q_rAGxo";
-export const videoTitle = "one iced latte w ur milk pls";
+export const videoDate = new Date("2025-04-08T23:51:48Z");
+export const videoHref = "https://www.youtube.com/watch?v=PrENYq4nPQE";
+export const videoTitle = "lonely";
 
 export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=-4H-3KQ4BMA","title":"vivariasmp opening!!!!","thumbnail":"https://i.ytimg.com/vi/-4H-3KQ4BMA/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=j5md62QQv9A","title":"grim fandango w/@WalkyriaLiora","thumbnail":"https://i.ytimg.com/vi/j5md62QQv9A/mqdefault_live.jpg"}];
 
