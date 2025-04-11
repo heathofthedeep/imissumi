6f0b2a29a61d3a7a7d2773dbdedea640c95e2ae3
heathofthedeep <neap-noisome-2b@icloud.com>
2025-04-11 17:46:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-04-08T23:51:48Z");
 export const videoHref = "https://www.youtube.com/watch?v=PrENYq4nPQE";
 export const videoTitle = "lonely";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=p1r2vGcgZGo","title":"listen to my tummy while i eat","thumbnail":"https://i.ytimg.com/vi/p1r2vGcgZGo/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=-4H-3KQ4BMA","title":"vivariasmp opening!!!!","thumbnail":"https://i.ytimg.com/vi/-4H-3KQ4BMA/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=j5md62QQv9A","title":"grim fandango w/@WalkyriaLiora","thumbnail":"https://i.ytimg.com/vi/j5md62QQv9A/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=p1r2vGcgZGo","title":"listen to my tummy while i eat","thumbnail":"https://i.ytimg.com/vi/p1r2vGcgZGo/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=-4H-3KQ4BMA","title":"vivariasmp opening!!!!","thumbnail":"https://i.ytimg.com/vi/-4H-3KQ4BMA/mqdefault_live.jpg"}];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=j5md62QQv9A";
+export const liveTitle = "grim fandango w/@WalkyriaLiora";
