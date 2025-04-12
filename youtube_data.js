fb8defba8c59931fe6aaf18cc88e408496be87ae
heathofthedeep <neap-noisome-2b@icloud.com>
2025-04-12 14:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-04-12T01:42:01Z");
 export const videoHref = "https://www.youtube.com/watch?v=j5md62QQv9A";
 export const videoTitle = "grim fandango w/@WalkyriaLiora";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=p1r2vGcgZGo","title":"listen to my tummy while i eat","thumbnail":"https://i.ytimg.com/vi/p1r2vGcgZGo/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=-4H-3KQ4BMA","title":"vivariasmp opening!!!!","thumbnail":"https://i.ytimg.com/vi/-4H-3KQ4BMA/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=p1r2vGcgZGo","title":"listen to my tummy while i eat","thumbnail":"https://i.ytimg.com/vi/p1r2vGcgZGo/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
