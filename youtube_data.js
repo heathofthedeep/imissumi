d3c0f6cd60a0d3eb11f932b1e2532b2f54301804
heathofthedeep <neap-noisome-2b@icloud.com>
2025-04-20 14:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-04-15T23:54:24Z");
 export const videoHref = "https://www.youtube.com/watch?v=p1r2vGcgZGo";
 export const videoTitle = "listen to my tummy while i eat";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=hFr51tEMAeM","title":"hopping to give you my eggs! 🐇","thumbnail":"https://i.ytimg.com/vi/hFr51tEMAeM/mqdefault_live.jpg"}];
+export const upcomingVideos = [];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=hFr51tEMAeM";
+export const liveTitle = "hopping to give you my eggs! 🐇";
