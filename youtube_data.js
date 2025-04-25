0862049231120ad7fa9bdd65213a671124a505d7
heathofthedeep <neap-noisome-2b@icloud.com>
2025-04-25 01:46:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,8 +1,8 @@
 export const online = false;
     
-export const videoDate = new Date("2025-04-15T23:54:24Z");
-export const videoHref = "https://www.youtube.com/watch?v=p1r2vGcgZGo";
-export const videoTitle = "listen to my tummy while i eat";
+export const videoDate = new Date("2025-04-20T21:44:44Z");
+export const videoHref = "https://www.youtube.com/watch?v=hFr51tEMAeM";
+export const videoTitle = "hopping to give you my eggs! 🐇";
 
 export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=JClKu7msgd0","title":"bratty yandere breaking parole","thumbnail":"https://i.ytimg.com/vi/JClKu7msgd0/mqdefault_live.jpg"}];
 
