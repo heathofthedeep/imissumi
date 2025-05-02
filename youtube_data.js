c25014846334cee90873883a4d131d7c4c314656
heathofthedeep <neap-noisome-2b@icloud.com>
2025-05-02 02:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-04-20T21:44:44Z");
 export const videoHref = "https://www.youtube.com/watch?v=hFr51tEMAeM";
 export const videoTitle = "hopping to give you my eggs! 🐇";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=Qm404PqBIco","title":"i&#39;m back","thumbnail":"https://i.ytimg.com/vi/Qm404PqBIco/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=JClKu7msgd0","title":"bratty yandere breaking parole","thumbnail":"https://i.ytimg.com/vi/JClKu7msgd0/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=yg9PmxjOhOs","title":"gimme a kiss i&#39;m scared of mining | vivariasmp","thumbnail":"https://i.ytimg.com/vi/yg9PmxjOhOs/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=Qm404PqBIco","title":"i&#39;m back + small announcement!","thumbnail":"https://i.ytimg.com/vi/Qm404PqBIco/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=JClKu7msgd0","title":"bratty yandere breaking parole","thumbnail":"https://i.ytimg.com/vi/JClKu7msgd0/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
