8b024724d3a99a1793fddc49a457c260defb93bb
heathofthedeep <neap-noisome-2b@icloud.com>
2025-05-03 15:46:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-05-03T10:50:09Z");
 export const videoHref = "https://www.youtube.com/watch?v=Qm404PqBIco";
 export const videoTitle = "i&#39;m back + small announcement!";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=yg9PmxjOhOs","title":"gimme a kiss i&#39;m scared of mining | vivariasmp","thumbnail":"https://i.ytimg.com/vi/yg9PmxjOhOs/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=JClKu7msgd0","title":"bratty yandere breaking parole","thumbnail":"https://i.ytimg.com/vi/JClKu7msgd0/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=JClKu7msgd0","title":"bratty yandere breaking parole","thumbnail":"https://i.ytimg.com/vi/JClKu7msgd0/mqdefault_live.jpg"}];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=yg9PmxjOhOs";
+export const liveTitle = "gimme a kiss i&#39;m scared of mining | vivariasmp";
