fb197eea2a9448af004916f0f789655e33d10d56
heathofthedeep <neap-noisome-2b@icloud.com>
2025-05-09 13:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-05-04T01:34:06Z");
 export const videoHref = "https://www.youtube.com/watch?v=yg9PmxjOhOs";
 export const videoTitle = "gimme a kiss i&#39;m scared of mining | vivariasmp";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=enL20VuZGNk","title":"Tragical Girl - Umi Kyoku (Official Video)","thumbnail":"https://i.ytimg.com/vi/enL20VuZGNk/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=BrLfe1h8tGI","title":"this is technically not illegal | vivariasmp","thumbnail":"https://i.ytimg.com/vi/BrLfe1h8tGI/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=enL20VuZGNk","title":"Tragical Girl - Umi Kyoku (Official Video)","thumbnail":"https://i.ytimg.com/vi/enL20VuZGNk/mqdefault_live.jpg"}];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=BrLfe1h8tGI";
+export const liveTitle = "umi don&#39;t fall asleep on stream | #vivariasmp";
