18c9f39d1bf7ed07c2fdda8036b81f62c6ef869d
heathofthedeep <neap-noisome-2b@icloud.com>
2025-05-09 21:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-05-09T20:34:23Z");
 export const videoHref = "https://www.youtube.com/watch?v=BrLfe1h8tGI";
 export const videoTitle = "umi don&#39;t fall asleep on stream | #vivariasmp";
 
-export const upcomingVideos = [];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=_nCgkfYoBsw","title":"singing until we get to 300 likes (or i get bored)","thumbnail":"https://i.ytimg.com/vi/_nCgkfYoBsw/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
