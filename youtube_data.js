0a0a4a36c4e8db290bfd90c8898cfe0ece62fd6e
heathofthedeep <neap-noisome-2b@icloud.com>
2025-05-11 15:46:07 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-05-11T02:18:22Z");
 export const videoHref = "https://www.youtube.com/watch?v=_nCgkfYoBsw";
 export const videoTitle = "singing until we get to 461 likes (or i get bored)";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=Ns8tbzJCMc0","title":"happy mother&#39;s day","thumbnail":"https://i.ytimg.com/vi/Ns8tbzJCMc0/mqdefault_live.jpg"}];
+export const upcomingVideos = [];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=Ns8tbzJCMc0";
+export const liveTitle = "happy mother&#39;s day";
