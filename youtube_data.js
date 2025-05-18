39bf3b862c078989f5ac5efe52e4a56598d361d0
heathofthedeep <neap-noisome-2b@icloud.com>
2025-05-17 21:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,4 +1,4 @@
-export const online = true;
+export const online = false;
     
 export const videoDate = new Date("2025-05-14T00:45:16Z");
 export const videoHref = "https://www.youtube.com/watch?v=mCsYJ4B6Io4";
@@ -6,5 +6,5 @@ export const videoTitle = "this is a hole stream";
 
 export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=vxOXZDPiV_Q","title":"you wanted to get marked, didn&#39;t you?","thumbnail":"https://i.ytimg.com/vi/vxOXZDPiV_Q/mqdefault_live.jpg"}];
 
-export const liveHref = "https://www.youtube.com/watch?v=3QToR_-U6fI";
-export const liveTitle = "tragical girl karaoke";
+export const liveHref = "";
+export const liveTitle = "";
