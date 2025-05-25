7399890a5779da9368ea518909d26f9b76f9769e
heathofthedeep <neap-noisome-2b@icloud.com>
2025-05-24 19:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,4 +1,4 @@
-export const online = true;
+export const online = false;
     
 export const videoDate = new Date("2025-05-21T19:51:08Z");
 export const videoHref = "https://www.youtube.com/watch?v=ro-iJUt30Wc";
@@ -6,5 +6,5 @@ export const videoTitle = "I&#39;M UMI KYOKU ✨";
 
 export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=hzqQXu3qBdI","title":"another announcement!?","thumbnail":"https://i.ytimg.com/vi/hzqQXu3qBdI/mqdefault_live.jpg"}];
 
-export const liveHref = "https://www.youtube.com/watch?v=xV-STSt06s8";
-export const liveTitle = "i&#39;m scared of how big my hole is";
+export const liveHref = "";
+export const liveTitle = "";
