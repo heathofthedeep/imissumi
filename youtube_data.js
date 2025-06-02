934a4ac4bfc27bf56c9fc88029066ccba8ec5884
heathofthedeep <neap-noisome-2b@icloud.com>
2025-06-01 22:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-05-25T22:03:30Z");
 export const videoHref = "https://www.youtube.com/watch?v=hzqQXu3qBdI";
 export const videoTitle = "another announcement!?";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=nLfvsXmADOo","title":"TRYING OUT 5 NEW STEAM DEMOS!","thumbnail":"https://i.ytimg.com/vi/nLfvsXmADOo/mqdefault_live.jpg"}];
+export const upcomingVideos = [];
 
 export const liveHref = "";
 export const liveTitle = "";
