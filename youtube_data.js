ea34dd3cd019843993d10454c29f9050a5f57df5
heathofthedeep <neap-noisome-2b@icloud.com>
2025-06-06 16:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-05-25T22:03:30Z");
 export const videoHref = "https://www.youtube.com/watch?v=hzqQXu3qBdI";
 export const videoTitle = "another announcement!?";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=La-zb-OgoQk","title":"karaoke to practice for dokomi!!!!","thumbnail":"https://i.ytimg.com/vi/La-zb-OgoQk/mqdefault_live.jpg"}];
+export const upcomingVideos = [];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=La-zb-OgoQk";
+export const liveTitle = "karaoke to practice for dokomi!!!!";
