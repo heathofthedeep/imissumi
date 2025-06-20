3111416c506adfb1b6636a9cce72ab7df6b72f0a
heathofthedeep <neap-noisome-2b@icloud.com>
2025-06-20 17:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
 export const online = false;
     
-export const videoDate = new Date("2025-06-14T16:00:09Z");
-export const videoHref = "https://www.youtube.com/watch?v=6pJPcd0mruc";
-export const videoTitle = "my endless blue (First EP Preview)";
+export const videoDate = new Date("2025-06-20T22:00:06Z");
+export const videoHref = "https://www.youtube.com/watch?v=Syz_HYffrqY";
+export const videoTitle = "【COVER】 SPARKS - Takanashi Kiara (Rock Version)【Umi Kyoku】";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=Syz_HYffrqY","title":"【COVER】 SPARKS - Takanashi Kiara (Rock Version)【Umi Kyoku】","thumbnail":"https://i.ytimg.com/vi/Syz_HYffrqY/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=Uax2VzbOzxo","title":"it&#39;s not goodbye, it&#39;s an i&#39;ll see you l8tr!","thumbnail":"https://i.ytimg.com/vi/Uax2VzbOzxo/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
