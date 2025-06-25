c33fd064fdd1d122309c761ce61dd2618f9fbc6b
heathofthedeep <neap-noisome-2b@icloud.com>
2025-06-25 17:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,8 +1,8 @@
 export const online = false;
     
-export const videoDate = new Date("2025-06-20T22:00:06Z");
-export const videoHref = "https://www.youtube.com/watch?v=Syz_HYffrqY";
-export const videoTitle = "【COVER】 SPARKS - Takanashi Kiara (Rock Version)【Umi Kyoku】";
+export const videoDate = new Date("2025-06-25T00:29:41Z");
+export const videoHref = "https://www.youtube.com/watch?v=ys2N9EVDpSA";
+export const videoTitle = "my endless blue (musical experience)";
 
 export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=Uax2VzbOzxo","title":"it&#39;s not goodbye, it&#39;s an i&#39;ll see you l8tr!","thumbnail":"https://i.ytimg.com/vi/Uax2VzbOzxo/mqdefault_live.jpg"}];
 
