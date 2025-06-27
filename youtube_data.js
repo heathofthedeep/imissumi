73fece070854d535828ad7b57ecaf83370e5b096
heathofthedeep <neap-noisome-2b@icloud.com>
2025-06-27 16:46:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-06-26T09:29:34Z");
 export const videoHref = "https://www.youtube.com/watch?v=ys2N9EVDpSA";
 export const videoTitle = "my endless blue (musical experience)";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=Uax2VzbOzxo","title":"it&#39;s not goodbye, it&#39;s an i&#39;ll see you l8tr!","thumbnail":"https://i.ytimg.com/vi/Uax2VzbOzxo/mqdefault_live.jpg"}];
+export const upcomingVideos = [];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=Uax2VzbOzxo";
+export const liveTitle = "it&#39;s not goodbye, it&#39;s an i&#39;ll see you l8tr!";
