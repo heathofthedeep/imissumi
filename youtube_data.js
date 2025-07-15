2934a5bcbaa28f5050879119ab7f2c166dae068d
heathofthedeep <neap-noisome-2b@icloud.com>
2025-07-15 14:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,8 +1,8 @@
 export const online = false;
     
-export const videoDate = new Date("2025-07-06T05:59:50Z");
-export const videoHref = "https://www.youtube.com/watch?v=Becq4DPjFNM";
-export const videoTitle = "Ain’t It Fun #shorts";
+export const videoDate = new Date("2025-07-15T19:12:30Z");
+export const videoHref = "https://www.youtube.com/watch?v=AqKB-W4AfFw";
+export const videoTitle = "I have nothing… #shorts";
 
 export const upcomingVideos = [];
 
