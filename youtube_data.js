a8071c34a29c5b7a67670462e5e94a4ead0b1322
heathofthedeep <neap-noisome-2b@icloud.com>
2025-07-22 15:46:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
 export const online = false;
     
-export const videoDate = new Date("2025-07-21T00:39:17Z");
-export const videoHref = "https://www.youtube.com/watch?v=ru6rp6XSQS8";
-export const videoTitle = "What Happened? #shorts";
+export const videoDate = new Date("2025-07-22T20:30:06Z");
+export const videoHref = "https://www.youtube.com/watch?v=kz47hNjWjlU";
+export const videoTitle = "【COVER】Hare Hare Yukai - The Melancholy of Haruhi Suzumiya";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=kz47hNjWjlU","title":"【COVER】Hare Hare Yukai - The Melancholy of Haruhi Suzumiya","thumbnail":"https://i.ytimg.com/vi/kz47hNjWjlU/mqdefault_live.jpg"}];
+export const upcomingVideos = [];
 
 export const liveHref = "";
 export const liveTitle = "";
