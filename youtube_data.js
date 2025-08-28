81670232e9a70515240241c12c9c8e8d802d6b8c
heathofthedeep <neap-noisome-2b@icloud.com>
2025-08-28 11:16:07 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-08-16T02:14:27Z");
 export const videoHref = "https://www.youtube.com/watch?v=KjSSm03pOVc";
 export const videoTitle = "happy birthday to me!!!!!";
 
-export const upcomingVideos = [];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=n8EFhC_3gsk","title":"FINTASIA 2025","thumbnail":"https://i.ytimg.com/vi/n8EFhC_3gsk/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
