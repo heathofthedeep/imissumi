d7bcc396d30f06742e5b5f7d3b4a5417f92e1662
heathofthedeep <neap-noisome-2b@icloud.com>
2025-09-01 08:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-08-31T03:56:06Z");
 export const videoHref = "https://www.youtube.com/watch?v=n8EFhC_3gsk";
 export const videoTitle = "FINTASIA 2025";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=M-qJpqV56n8","title":"SING FOR A WISH KARAOKE RELAY DAY 3","thumbnail":"https://i.ytimg.com/vi/M-qJpqV56n8/mqdefault_live.jpg"}];
+export const upcomingVideos = [];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=M-qJpqV56n8";
+export const liveTitle = "SING FOR A WISH KARAOKE RELAY DAY 3";
