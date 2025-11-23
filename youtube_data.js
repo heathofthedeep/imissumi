bbc57eabe79f02031de9576976cc7de8fd226edb
heathofthedeep <neap-noisome-2b@icloud.com>
2025-11-22 22:16:05 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-11-18T02:17:17Z");
 export const videoHref = "https://www.youtube.com/watch?v=1ofDav_Wb6o";
 export const videoTitle = "she&#39;s a feesh";
 
-export const upcomingVideos = [];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=-2JWOZtqVQ8","title":"I SANG AT HYPERJAPAN MANCHESTER 2025","thumbnail":"https://i.ytimg.com/vi/-2JWOZtqVQ8/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
