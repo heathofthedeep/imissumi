590eee8e579625266da73cb0fc0a3a71d3c883c2
heathofthedeep <neap-noisome-2b@icloud.com>
2025-11-30 11:16:05 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-11-23T20:00:06Z");
 export const videoHref = "https://www.youtube.com/watch?v=-2JWOZtqVQ8";
 export const videoTitle = "I SANG AT HYPERJAPAN MANCHESTER 2025";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=nush-7pBPSI","title":"THERE’S A NEOPETS TRADING CARD GAME?!","thumbnail":"https://i.ytimg.com/vi/nush-7pBPSI/mqdefault_live.jpg"}];
+export const upcomingVideos = [];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=nush-7pBPSI";
+export const liveTitle = "THERE’S A NEOPETS TRADING CARD GAME?!";
