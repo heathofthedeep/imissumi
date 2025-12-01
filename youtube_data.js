0e706e8e7bdd84f4f88897c076473758f8c06d47
heathofthedeep <neap-noisome-2b@icloud.com>
2025-12-01 01:16:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,6 +1,6 @@
 export const online = false;
     
-export const videoDate = new Date("2025-11-30T14:46:39Z");
+export const videoDate = new Date("2025-12-01T07:12:17Z");
 export const videoHref = "https://www.youtube.com/watch?v=nush-7pBPSI";
 export const videoTitle = "THERE’S A NEOPETS TRADING CARD GAME?!";
 
