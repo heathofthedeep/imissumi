8881854bd471487cee68e93341f3a13d07c67d1c
heathofthedeep <neap-noisome-2b@icloud.com>
2025-12-08 14:16:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = false;
+export const online = true;
     
 export const videoDate = new Date("2025-12-01T07:12:17Z");
 export const videoHref = "https://www.youtube.com/watch?v=nush-7pBPSI";
 export const videoTitle = "THERE’S A NEOPETS TRADING CARD GAME?!";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=TbAjCUKB8-s","title":"she&#39;s just a silly southern girl #vtrp","thumbnail":"https://i.ytimg.com/vi/TbAjCUKB8-s/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=sSsQcKkbc1w","title":"ANITA DIAMOND IS BACK #vtrp","thumbnail":"https://i.ytimg.com/vi/sSsQcKkbc1w/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=TbAjCUKB8-s","title":"she&#39;s just a silly southern girl #vtrp","thumbnail":"https://i.ytimg.com/vi/TbAjCUKB8-s/mqdefault_live.jpg"}];
 
-export const liveHref = "";
-export const liveTitle = "";
+export const liveHref = "https://www.youtube.com/watch?v=sSsQcKkbc1w";
+export const liveTitle = "HOLOLIVE HAS A TCG??????";
