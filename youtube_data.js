286598a0e45739d6f453d9c045b6c2bf85a8a23e
heathofthedeep <neap-noisome-2b@icloud.com>
2025-12-08 18:16:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = true;
+export const online = false;
     
-export const videoDate = new Date("2025-12-01T07:12:17Z");
-export const videoHref = "https://www.youtube.com/watch?v=nush-7pBPSI";
-export const videoTitle = "THERE’S A NEOPETS TRADING CARD GAME?!";
+export const videoDate = new Date("2025-12-08T23:55:51Z");
+export const videoHref = "https://www.youtube.com/watch?v=sSsQcKkbc1w";
+export const videoTitle = "HOLOLIVE HAS A TCG??????";
 
 export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=TbAjCUKB8-s","title":"she&#39;s just a silly southern girl #vtrp","thumbnail":"https://i.ytimg.com/vi/TbAjCUKB8-s/mqdefault_live.jpg"}];
 
-export const liveHref = "https://www.youtube.com/watch?v=sSsQcKkbc1w";
-export const liveTitle = "HOLOLIVE HAS A TCG??????";
+export const liveHref = "";
+export const liveTitle = "";
