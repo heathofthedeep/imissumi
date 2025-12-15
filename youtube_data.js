f7b4464247ea52bbbf8817a0894fd30a7dabfc61
heathofthedeep <neap-noisome-2b@icloud.com>
2025-12-14 19:16:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-12-08T23:55:51Z");
 export const videoHref = "https://www.youtube.com/watch?v=sSsQcKkbc1w";
 export const videoTitle = "HOLOLIVE HAS A TCG??????";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=TbAjCUKB8-s","title":"she&#39;s just a silly southern girl #vtrp","thumbnail":"https://i.ytimg.com/vi/TbAjCUKB8-s/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=BszJWNpUE8I","title":"Tragical Girl - Umi Kyoku (#Idolatry3D Performance)","thumbnail":"https://i.ytimg.com/vi/BszJWNpUE8I/mqdefault_live.jpg"},{"url":"https://www.youtube.com/watch?v=TbAjCUKB8-s","title":"she&#39;s just a silly southern girl #vtrp","thumbnail":"https://i.ytimg.com/vi/TbAjCUKB8-s/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
