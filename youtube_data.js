74f24867706be1bd3fc64e2dc0e182a0201d6ed7
heathofthedeep <neap-noisome-2b@icloud.com>
2025-12-18 01:46:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2025-12-15T21:00:06Z");
 export const videoHref = "https://www.youtube.com/watch?v=BszJWNpUE8I";
 export const videoTitle = "Tragical Girl - Umi Kyoku (#Idolatry3D Performance)";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=TbAjCUKB8-s","title":"she&#39;s just a silly southern girl #vtrp","thumbnail":"https://i.ytimg.com/vi/TbAjCUKB8-s/mqdefault_live.jpg"}];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=jXIF2c89Nvw","title":"HOLIDAY KARAOKE","thumbnail":"https://i.ytimg.com/vi/jXIF2c89Nvw/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
