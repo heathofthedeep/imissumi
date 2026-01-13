fffdd742be7179ae79471708c375f83621b6fe1f
heathofthedeep <neap-noisome-2b@icloud.com>
2026-01-12 18:46:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,6 +1,6 @@
 export const online = false;
     
-export const videoDate = new Date("2026-01-12T17:46:29Z");
+export const videoDate = new Date("2026-01-13T00:21:10Z");
 export const videoHref = "https://www.youtube.com/watch?v=u1P6DY1S0dg";
 export const videoTitle = "3D UWUMI KARAOKE";
 
