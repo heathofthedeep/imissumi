fe74db1c9430421fb6198ca2288613dc83f33b6b
heathofthedeep <neap-noisome-2b@icloud.com>
2026-02-05 16:16:08 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2026-01-30T00:38:16Z");
 export const videoHref = "https://www.youtube.com/watch?v=OaYWMtMe7UE";
 export const videoTitle = "umi kyoku karaoke";
 
-export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=SGsoza54KWM","title":"HOLOLIVE TCG BOX OPENING WAHOO!!!","thumbnail":"https://i.ytimg.com/vi/SGsoza54KWM/mqdefault_live.jpg"}];
+export const upcomingVideos = [];
 
 export const liveHref = "";
 export const liveTitle = "";
