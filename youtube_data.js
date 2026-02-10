37e9202781cfe553816479e91e4a99a03a82ea93
heathofthedeep <neap-noisome-2b@icloud.com>
2026-02-10 12:46:07 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2026-01-30T00:38:16Z");
 export const videoHref = "https://www.youtube.com/watch?v=OaYWMtMe7UE";
 export const videoTitle = "umi kyoku karaoke";
 
-export const upcomingVideos = [];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=V608OtgRJ3k","title":"【lucid Karaoke Relay】i hate valentine&#39;s day but i like to sing 💞","thumbnail":"https://i.ytimg.com/vi/V608OtgRJ3k/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
