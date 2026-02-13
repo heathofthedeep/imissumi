83c494d35619ed1e63dba98220b2727f84cdc8ea
heathofthedeep <neap-noisome-2b@icloud.com>
2026-02-12 20:16:06 -0600

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,6 +1,6 @@
 export const online = false;
     
-export const videoDate = new Date("2026-02-10T18:37:03Z");
+export const videoDate = new Date("2026-02-13T01:48:31Z");
 export const videoHref = "https://www.youtube.com/watch?v=V608OtgRJ3k";
 export const videoTitle = "【lucid Karaoke Relay】i hate valentine&#39;s day but i like to sing 💞";
 
