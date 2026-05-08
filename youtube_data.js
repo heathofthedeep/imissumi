7621e79d63ccd3a76f7f5821d6a3f1faaf7d1cec
heathofthedeep <neap-noisome-2b@icloud.com>
2026-05-08 16:46:09 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2026-04-08T22:57:53Z");
 export const videoHref = "https://www.youtube.com/watch?v=4gaLrypf2Kc";
 export const videoTitle = "AQUARIUM FIELD TRIP 🦈";
 
-export const upcomingVideos = [];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=nq8QpAhKcdw","title":"I LOVE WOMEN!!!!!","thumbnail":"https://i.ytimg.com/vi/nq8QpAhKcdw/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
