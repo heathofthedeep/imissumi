639300307f55c7bb79c295b27bb34868f395d17c
heathofthedeep <neap-noisome-2b@icloud.com>
2026-08-31 16:46:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2026-07-31T19:06:41Z");
 export const videoHref = "https://www.youtube.com/watch?v=6iDibKiRNQE";
 export const videoTitle = "lunch and karaoke for u !";
 
-export const upcomingVideos = [];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=4cSXkduArmI","title":"yap yap yap yap yap w uwumi","thumbnail":"https://i.ytimg.com/vi/4cSXkduArmI/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
