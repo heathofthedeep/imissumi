4aee821778e0aca6a6555774916b129e7921efd6
heathofthedeep <neap-noisome-2b@icloud.com>
2026-08-31 22:16:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -1,10 +1,10 @@
-export const online = true;
+export const online = false;
     
-export const videoDate = new Date("2026-08-12T20:14:08Z");
-export const videoHref = "https://www.youtube.com/watch?v=XUcyu3oRzXM";
-export const videoTitle = "UWUMI BIRTHDAY!!!!";
+export const videoDate = new Date("2026-09-01T02:57:12Z");
+export const videoHref = "https://www.youtube.com/watch?v=4cSXkduArmI";
+export const videoTitle = "yap yap yap yap yap w uwumi";
 
 export const upcomingVideos = [];
 
-export const liveHref = "https://www.youtube.com/watch?v=4cSXkduArmI";
-export const liveTitle = "yap yap yap yap yap w uwumi";
+export const liveHref = "";
+export const liveTitle = "";
