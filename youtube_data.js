3bbd80fa60b9915217e1520e4bd5f562b732c194
heathofthedeep <neap-noisome-2b@icloud.com>
2026-09-22 20:46:06 -0500

    This is the commit message generated via GitHub API

--- youtube_data.js
+++ youtube_data.js
@@ -4,7 +4,7 @@ export const videoDate = new Date("2026-09-01T02:57:12Z");
 export const videoHref = "https://www.youtube.com/watch?v=4cSXkduArmI";
 export const videoTitle = "yap yap yap yap yap w uwumi";
 
-export const upcomingVideos = [];
+export const upcomingVideos = [{"url":"https://www.youtube.com/watch?v=EXA_vdGBq7A","title":"uwumi wednesday","thumbnail":"https://i.ytimg.com/vi/EXA_vdGBq7A/mqdefault_live.jpg"}];
 
 export const liveHref = "";
 export const liveTitle = "";
